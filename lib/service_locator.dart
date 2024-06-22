007dab4828d53c7ab050ffaab4e78b8f45e67b1b
sherifhasan <sherif.hasan1994@gmail.com>
2024-06-22 16:22:37 +0200

    remove token

--- lib/service_locator.dart
+++ lib/service_locator.dart
@@ -6,6 +6,7 @@ import 'package:kanban_board/infrastructure/datasource/tasks_datasource.dart';
 import 'package:kanban_board/infrastructure/task_repository_impl.dart';
 
 final sl = GetIt.instance;
+const apiToken = 'add yours';
 
 void setup() {
   // Register Dio
@@ -13,7 +14,7 @@ void setup() {
         baseUrl: 'https://api.todoist.com/rest/v2',
         headers: {
           'Content-Type': 'application/json',
-          'Authorization': 'Bearer cc8418c0023fa88a02634791be614aeaef3f6770',
+          'Authorization': 'Bearer $apiToken',
         },
       )));
 
