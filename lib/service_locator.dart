94ec90c82ee8ed552b6405ef306b331758093f76
sherifhasan <sherif.hasan1994@gmail.com>
2024-06-22 16:11:37 +0200

    [Add] : add service locator for DI

---
+++ lib/service_locator.dart
@@ -0,0 +1,27 @@
+import 'package:get_it/get_it.dart';
+import 'package:dio/dio.dart';
+import 'package:kanban_board/domain/task_repository.dart';
+import 'package:kanban_board/infrastructure/datasource/remote_tasks_data_source_impl.dart';
+import 'package:kanban_board/infrastructure/datasource/tasks_datasource.dart';
+import 'package:kanban_board/infrastructure/task_repository_impl.dart';
+
+final sl = GetIt.instance;
+
+void setup() {
+  // Register Dio
+  sl.registerLazySingleton<Dio>(() => Dio(BaseOptions(
+        baseUrl: 'https://api.todoist.com/rest/v2',
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': 'Bearer cc8418c0023fa88a02634791be614aeaef3f6770',
+        },
+      )));
+
+  // Register TasksDataSource
+  sl.registerLazySingleton<TasksDataSource>(
+      () => RemoteTasksDataSourceImpl(dio: sl<Dio>()));
+
+  // Register TaskRepository
+  sl.registerLazySingleton<TaskRepository>(
+      () => TaskRepositoryImpl(remoteTasksDataSource: sl<TasksDataSource>()));
+}
