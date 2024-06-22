fb4bba8888a3ab0c7e2a8be4d89fff47a3bac5dd
sherifhasan <sherif.hasan1994@gmail.com>
2024-06-22 17:18:41 +0200

    remove token

--- lib/service_locator.dart
+++ lib/service_locator.dart
@@ -6,7 +6,7 @@ import 'package:kanban_board/infrastructure/datasource/tasks_datasource.dart';
 import 'package:kanban_board/infrastructure/task_repository_impl.dart';
 
 final sl = GetIt.instance;
-const apiToken = 'cc8418c0023fa88a02634791be614aeaef3f6770';
+const apiToken = 'add your token';
 
 void setup() {
   // Register Dio
