37593afec7b1db258ba842e1b3e8e3dc6ea87e51
sherifhasan <sherif.hasan1994@gmail.com>
2024-06-23 18:29:20 +0200

    update my app to accept multiBloc

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:kanban_board/application/features/tasks/tasks_cubit.dart';
+import 'package:kanban_board/presentation/main/main_screen.dart';
 import 'package:kanban_board/service_locator.dart';
 
 void main() {
@@ -13,14 +15,22 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Kanban Board',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: BlocProvider(
-        create: (_) => sl<TasksCubit>()..loadTasks(''),
-        child: Container(),
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.landscapeLeft,
+      DeviceOrientation.landscapeRight,
+    ]);
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(
+          create: (_) => sl<TasksCubit>()..loadTasks(''),
+        ),
+      ],
+      child: MaterialApp(
+        title: 'Kanban Board',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: const MainScreen(),
       ),
     );
   }
